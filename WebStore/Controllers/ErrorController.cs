4a95e0dd3eb56e735e8abcf4c9a089adc4693f7b
Gribanoid <grisha2002grisha@yandex.com>
2021-08-08 02:56:36 +0300

    Добавление страниц

---
+++ WebStore/Controllers/ErrorController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -5,6 +5,17 @@ namespace WebStore.Controllers
     public class HomeController : Controller
     {        
         public IActionResult Index() => View();
+        public IActionResult Error() => View();
+        public IActionResult Blog() => View();
+        public IActionResult BlogSingle() => View();
+        public IActionResult Cart() => View();
+        public IActionResult Checkout() => View();
+        public IActionResult ContactUs() => View();
+        public IActionResult Login() => View();
+        public IActionResult ProductDetails() => View();
+        public IActionResult Shop() => View();
         public IActionResult SecondAction(string id) => Content($"Action with value id: {id}");
+
+
     }
 }
