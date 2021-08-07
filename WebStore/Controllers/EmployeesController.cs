e14b214f7e0ceb66df2eedafe71de60fd81f9e19
Gribanoid <grisha2002grisha@yandex.com>
2021-08-07 23:45:00 +0300

    Контроллер Сотрудников

---
+++ WebStore/Controllers/EmployeesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private static readonly List<Employee> __Employees = new()
+        {
+            new Employee { Id = 1, LastName = "Николаев", FirstName = "Григорий", Patronymic = "Андреевич", Age = 18 },
+            new Employee { Id = 2, LastName = "Соловьев", FirstName = "Георгий", Patronymic = "Евгеньевич", Age = 19 },
+            new Employee { Id = 3, LastName = "Романов", FirstName = "Нияз", Patronymic = "Нияз", Age = 21 }
+        };
+        public IActionResult Index() => View(__Employees);
+    }
+}
